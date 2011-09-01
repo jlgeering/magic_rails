f51ccf8c19b5e35e504c68fde6b1974db18c5e70
Jean-Luc Geering <jlgeering.11@gmail.com>
2011-09-01 08:29:30 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MagicRails::Application.config.secret_token = 'cffc8541d32d0a20f68e7048985e06bb110c9c0f6e52dee47e019d8aedcbcbd60ab1df71807034e24a4fd2208cba3f9b912a99e93007e6f7058246172a56da47'
